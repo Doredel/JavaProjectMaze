b299d2c2dcb7a1d6af29914ae57606beb1c4a4cb
Dor Edelstein <doredel98@gmail.com>
2015-12-10 23:45:36 +0200

    I forgot those in the prev commit

---
+++ JavaProject/src/controller/DisplayCommand.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class DisplayCommand extends CommonCommand {
+
+	@Override
+	public void doCommand() {
+		// TODO Auto-generated method stub
+		displayMaze(input);
+	}
+
+	private void displayMaze(String input) {
+		
+	}
+
+}
---
+++ JavaProject/src/controller/Generate3DMazeCommand.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class Generate3DMazeCommand extends CommonCommand {
+
+	@Override
+	public void doCommand() {
+		
+	}
+
+}
