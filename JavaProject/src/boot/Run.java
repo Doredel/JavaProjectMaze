099759eb10d217691ca521000ce43239af75e795
Dor Edelstein <Doredel98@gmail.com>
2015-12-28 03:46:34 +0200

    Started Gui

--- JavaProject/src/boot/Run.java
+++ JavaProject/src/boot/Run.java
@@ -3,19 +3,24 @@ package boot;
 import algorithms.mazeGenerators.Position;
 import model.MyModel;
 import presenter.Presenter;
+import view.BasicWindow;
 import view.MyView;
+import view.PropertiesWindow;
+
 
 public class Run {
 	public static void main(String[] args) {
 		
-		MyView<Position> view = new MyView<Position>(); 
+		/*MyView<Position> view = new MyView<Position>(); 
 		MyModel model = new MyModel();
 		
 		Presenter<Position> presenter = new Presenter<Position>(model, view);
 		model.addObserver(presenter);
 		view.addObserver(presenter);
 		
-		view.start();	
+		view.start();	*/
+		BasicWindow bw = new PropertiesWindow(400, 200);
+		bw.run();
 	}
 }
 
