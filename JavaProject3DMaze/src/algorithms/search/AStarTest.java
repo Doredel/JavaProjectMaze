f9fee477e5d39f47fb14c13f694d3288ec1fb5ea
Dor Edelstein <Doredel98@gmail.com>
2016-01-03 20:59:22 +0200

    need to refresh the algorithms_IO jar(added the AStar test).

--- JavaProject3DMaze/src/algorithms/search/AStarTest.java
+++ JavaProject3DMaze/src/algorithms/search/AStarTest.java
@@ -2,24 +2,42 @@ package algorithms.search;
 
 import org.junit.Test;
 
+import algorithms.demo.Maze3dAdaptor;
 import algorithms.mazeGenerators.Maze3d;
 import algorithms.mazeGenerators.Position;
 
 public class AStarTest {
-
-	public Maze3d maze;
+public Maze3d maze;
 	
 	public AStarTest() {
 		maze = new Maze3d();
 		int [][][] arr = {{{0,1,1,0,1,1},{0,0,0,0,0,0},{1,1,1,0,1,0}},{{1,1,1,1,1,1},{1,1,1,0,1,0},{1,0,0,0,0,0}}};
 		maze.setMaze3d(arr);
-		maze.setStartPosition(new Position(0,0,0));
+		maze.setStartPosition(new Position(444,444,444));
 		maze.setGoalPosition(new Position(1,1,2));
+		MazeManhattanDistance check= new MazeManhattanDistance();
+				
+		double result;
 		
+		result = check.h(new State<Position>(maze.getStartPosition()),new State<Position>(maze.getGoalPosition()));
+		System.out.println("result: "+result);
+		
+		result = check.h(null, null);
+		System.out.println("result: "+result);
+		
+		result = check.h(new State<Position>(new Position(7,7,7)),new State<Position>(new Position(10,10,10)));
+		System.out.println("result: "+result);
+		
+		AStar<Position> a = new AStar<Position>(new MazeManhattanDistance());
+		result= a.CostForState(new State<Position>(maze.getStartPosition()),new State<Position>(maze.getStartPosition().getBackward()),new State<Position>(maze.getStartPosition().getForward()));
+		System.out.println("result: "+result);
+		Searcher<Position> searcher = new AStar<Position>(new MazeAirDistance());
+		searcher.search(new Maze3dAdaptor(maze));
 	}
 	
 	@Test
 	public void testSearch() {
+		
 	}
 
 }
