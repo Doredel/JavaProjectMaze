16570361e571a189d5efb8ebfa2cd51c6d269d4f
I5 <I5@I5>
2016-01-27 01:29:13 +0200

    can now generate a database only for the username:root and the password:1234

---
+++ ServerJavaProject/ServerJavaProject/src/boot/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package boot;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DatabaseConfig {
+	public static void main(String [] args)
+	{
+
+		Connection connection;
+		Statement stmt = null;
+
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "1234");
+			stmt = connection.createStatement();
+		    stmt.executeUpdate("CREATE DATABASE MAZE3D;");
+		    
+		    connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/maze3d", "root", "1234");
+			stmt = connection.createStatement();
+		    stmt.executeUpdate("CREATE TABLE MAZECACHE(NAME varchar(20),MAZE blob,SOLUTION blob);");
+		
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+	}
+}
